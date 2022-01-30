aacdcac878c3c3f0f883927224dc80ff548f162b
DanielLinux <danielcarvalhorst@gmail.com>
2022-01-30 12:11:04 -0300

    Finalizado Post Participant

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const server = express();
 server.use(cors());
 server.use(json());
 
+const userSchema = joi.object({
+    user: joi.string().required(),
+  });
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
@@ -29,10 +33,22 @@ server.get('/participants',async (req,res)=>{
 
 server.post('/participants',async (req,res)=>{
     try{
-
+        const participantOnline = await db.collection('participants').findOne({ user: req.body.user })
+        const validation = userSchema.validate(req.body, { abortEarly: true });
+        if(validation.error){res.sendStatus(422); return}
+        else if(participantOnline){res.send(409); return}
+        let dateNow= Date.now();
+        // let dateConvert= `${new Date(dateNow).getHours()}:${new Date(dateNow).getMinutes()}:${new Date(dateNow).getSeconds()}`
+        let dateConvertHour= new Date(dateNow).toTimeString().split(' ')[0]
+        let statusMessage={"from": req.body.user, "to": 'Todos', "text": 'entra na sala...', "type": 'status', "time": dateConvertHour}
+        let newParticipant = { "user":req.body.user, "lastStatus":dateNow}
+        await db.collection('participants').insertOne(newParticipant)
+        await db.collection('messages').insertOne(statusMessage)
+        res.sendStatus(201)
     }
     catch(error){
-
+        console.error(error);
+        res.sendStatus(500);
     }
 })
 
