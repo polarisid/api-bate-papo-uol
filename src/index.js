12a9ed4fc32ae4ef3e53ec9fca2464e2f7eeae33
DanielLinux <danielcarvalhorst@gmail.com>
2022-01-30 18:21:45 -0300

    Rota /status finalizada

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ server.use(cors());
 server.use(json());
 
 const userSchema = joi.object({
-    user: joi.string().required(),
+    name: joi.string().required(),
   });
 const messageSchema = joi.object({
     to: joi.string().required(),
@@ -38,14 +38,14 @@ server.get('/participants',async (req,res)=>{
 
 server.post('/participants',async (req,res)=>{
     try{
-        const participantOnline = await db.collection('participants').findOne({ user: req.body.user })
+        const participantOnline = await db.collection('participants').findOne({ name: req.body.name })
         const validation = userSchema.validate(req.body, { abortEarly: true });
         if(validation.error){res.sendStatus(422); return}
         else if(participantOnline){res.sendStatus(409); return}
         let dateNow= Date.now();
         let dateConvertHour= new Date(dateNow).toTimeString().split(' ')[0]
-        let statusMessage={"from": req.body.user, "to": 'Todos', "text": 'entra na sala...', "type": 'status', "time": dateConvertHour}
-        let newParticipant = { "user":req.body.user, "lastStatus":dateNow}
+        let statusMessage={"from": req.body.name, "to": 'Todos', "text": 'entra na sala...', "type": 'status', "time": dateConvertHour}
+        let newParticipant = { "name":req.body.name, "lastStatus":dateNow}
         await db.collection('participants').insertOne(newParticipant)
         await db.collection('messages').insertOne(statusMessage)
         res.sendStatus(201)
@@ -60,7 +60,7 @@ server.get('/messages', async (req,res)=>{
     let limiter = parseInt(req.query.limiter);
     const user = req.headers.user; 
     try{
-        const messages =await db.collection('messages').find({$or:[{"to":"Todos"},{"to":user}]}).toArray();
+        const messages =await db.collection('messages').find({$or:[{"to":"Todos"},{"to":user},{"from":user}]}).toArray();
         {limiter<messages.length? res.send(messages.slice(messages.length-limiter).slice(0)):res.send(messages)}
         //res.send(messages.slice(messages.length-10).slice(0)) //..reverse()
         //res.send(messages)
@@ -72,18 +72,20 @@ server.get('/messages', async (req,res)=>{
 })
 
 server.post('/messages',async (req,res)=>{
-    console.log(req.headers.user)
     let dateNow= Date.now();
     let dateConvertHour= new Date(dateNow).toTimeString().split(' ')[0]
     try{
-        const user = req.headers.user;
+        let user_logged = req.headers.user;
         // if (user===null || user===""||user===undefined){res.sendStatus(422);return }
-        const participantOnline = await db.collection('participants').findOne({ user: user })
-        if(participantOnline==null){res.sendStatus(422); return}
+        const participantOnline = await db.collection('participants').findOne({name:user_logged})
+        console.log(participantOnline)
+        if(participantOnline==null){res.sendStatus(422);console.log("erro1"); return}
+        
         const validation = messageSchema.validate(req.body, { abortEarly: true })
-        if(validation.error){res.sendStatus(422);return}
-        let message ={"from":user,...req.body,"time":dateConvertHour}
+        if(validation.error){res.sendStatus(422);console.log("erro2");return}
+        let message ={"from":user_logged,...req.body,"time":dateConvertHour}
         await db.collection('messages').insertOne(message)
+        
         res.sendStatus(201)
     }
     catch(error){
@@ -91,5 +93,20 @@ server.post('/messages',async (req,res)=>{
         res.sendStatus(500); 
     }
 })
+
+server.post('/status',async (req,res)=>{
+    try{
+        const user = req.headers.user;
+        const participantOnline = await db.collection('participants').findOne({ name: user })
+        if(participantOnline==null){res.sendStatus(404); return}
+        await db.collection('participants').updateOne({ name: user }, { $set: {lastStatus:Date.now()} });
+        res.sendStatus(200)
+    }
+    catch(error){
+        console.error(error);
+        res.sendStatus(500); 
+    }
+})
+
 
 server.listen(5000, ()=>{console.log("iniciado Server")});
