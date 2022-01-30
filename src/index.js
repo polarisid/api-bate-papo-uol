0d6db879f2787b4d87ede62f3d8cb6d11dcf9e81
DanielLinux <danielcarvalhorst@gmail.com>
2022-01-30 14:37:00 -0300

    Rota GET /messages

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ server.use(json());
 const userSchema = joi.object({
     user: joi.string().required(),
   });
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
+  });
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
@@ -36,7 +41,7 @@ server.post('/participants',async (req,res)=>{
         const participantOnline = await db.collection('participants').findOne({ user: req.body.user })
         const validation = userSchema.validate(req.body, { abortEarly: true });
         if(validation.error){res.sendStatus(422); return}
-        else if(participantOnline){res.send(409); return}
+        else if(participantOnline){res.sendStatus(409); return}
         let dateNow= Date.now();
         // let dateConvert= `${new Date(dateNow).getHours()}:${new Date(dateNow).getMinutes()}:${new Date(dateNow).getSeconds()}`
         let dateConvertHour= new Date(dateNow).toTimeString().split(' ')[0]
@@ -51,5 +56,21 @@ server.post('/participants',async (req,res)=>{
         res.sendStatus(500);
     }
 })
+
+server.get('/messages', async (req,res)=>{
+    let limiter = parseInt(req.query.limiter);
+    const user = req.headers.user; 
+    try{
+        const messages =await db.collection('messages').find({$or:[{"to":"Todos"},{"to":user}]}).toArray();
+        {limiter<messages.length? res.send(messages.slice(messages.length-limiter).slice(0)):res.send(messages)}
+        //res.send(messages.slice(messages.length-10).slice(0)) //..reverse()
+        //res.send(messages)
+    }
+    catch(error){
+        console.error(error);
+        res.sendStatus(500);
+    }
+})
+
 
 server.listen(5000, ()=>{console.log("iniciado Server")});
